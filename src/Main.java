0622db53bdc31e11c76273ede5d5b9d70137ac8e
cruzanab <ana.cruz@germinare.org.br>
2024-06-24 07:47:21 -0300

    Colocando coisas no arquivo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        int e;
+        int d;
+        int r;
+
+        e = input.nextInt();
+        d = input.nextInt();
+
+        if(e > d){
+            r = e + d;
+        }
+        else {
+            r = 2*(d - e);
+        }
+
+        System.out.println(r);
     }
 }
